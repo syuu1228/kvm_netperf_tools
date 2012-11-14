aedb03cb5c953517c1d569e684ce231326de80f7
Takuya ASADA <syuu@dokukino.com>
2012-11-15 05:15:18 +0900

    virt-top statistics

--- do_all.rb
+++ do_all.rb
@@ -48,6 +48,13 @@ c['cpus'].each do |cpu|
 			exit 1
 		end
 
+		puts "start virt-top_log"
+		ret = system("ssh #{c['host_ip']} screen -dm ~/kvm_netperf_tools/virt-top_log.sh #{v}-#{cpu}")
+		if !ret
+			puts ret
+			exit 1
+		end
+
 		puts "start netperf"
 		ret = system("#{File.dirname(__FILE__)}/multi_netperf.rb #{v} #{c['vm_ip_fmt']} #{c['vm_ip_start']} #{f} #{c['duration']} ~/netperf_%s.#{v}-#{cpu}.log")
 		if !ret
@@ -64,6 +71,13 @@ c['cpus'].each do |cpu|
 
 		puts "kill mpstat"
 		ret = system("ssh #{c['host_ip']} sudo killall mpstat")
+		if !ret
+			puts ret
+			exit 1
+		end
+
+		puts "kill virt-top"
+		ret = system("ssh #{c['host_ip']} sudo killall virt-top")
 		if !ret
 			puts ret
 			exit 1
---
+++ virt-top_log.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo virt-top --csv ~/virt-top.$1.log
