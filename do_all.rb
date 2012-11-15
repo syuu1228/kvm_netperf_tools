2caa8cbedf63e740f9c49896c553a64fdba6c5a2
Takuya ASADA <syuu@dokukino.com>
2012-11-16 03:20:48 +0900

    clear swap

--- do_all.rb
+++ do_all.rb
@@ -15,6 +15,18 @@ c['cpus'].each do |cpu|
 		next if File.exists?(File.expand_path("~/netperf_lat.#{v}-#{cpu}.log"))
 
 		(0...v).each do |j|
+			puts "clear swap"
+			ret = system("ssh #{c['host_ip']} sudo swapoff -a")
+			if !ret
+				puts ret
+				exit 1
+			end
+			ret = system("ssh #{c['host_ip']} sudo swapon -a")
+			if !ret
+				puts ret
+				exit 1
+			end
+
 			puts "start vm#{j}-#{cpu}"
 			ret = system("ssh #{c['host_ip']} sudo virsh start ubuntu#{j}-#{cpu}")
 			if !ret
