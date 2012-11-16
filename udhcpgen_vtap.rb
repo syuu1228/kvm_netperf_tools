dfeb0711a4a2f0a05acde44688dffb70f367cd0a
Takuya ASADA <syuu@dokukino.com>
2012-11-17 04:48:54 +0900

    udhcpgen fix

--- udhcpgen_vtap.rb
+++ udhcpgen_vtap.rb
@@ -4,10 +4,8 @@ require 'erb'
 
 c = YAML.load_file("#{File.dirname(__FILE__)}/config.yml")
 
-c['cpus'].each do |cpu|
-	(0...c['max_vms']).each do |no|
-		mac = sprintf(c['nic1_mac_fmt'], no)
-		ip = sprintf(c['vm_ip_fmt'], c['vm_ip_start_vtap'] + no)
-		puts "static_lease #{mac} #{ip}"
-	end
+(0...c['max_vms']).each do |no|
+	mac = sprintf(c['nic1_mac_fmt'], no)
+	ip = sprintf(c['vm_ip_fmt'], c['vm_ip_start_vtap'] + no)
+	puts "static_lease #{mac} #{ip}"
 end
