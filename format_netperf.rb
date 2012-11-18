881f66042831b46dd6cc209a70ae497f2945d7cd
Takuya ASADA <syuu@dokukino.com>
2012-11-19 08:34:23 +0900

    chmod

--- format_netperf.rb
+++ format_netperf.rb
@@ -18,6 +18,8 @@ min = File.new("netperf_#{IF}_min.csv", 'w')
 min.write HEADER
 max = File.new("netperf_#{IF}_max.csv", 'w')
 max.write HEADER
+bias = File.new("netperf_#{IF}_bias.csv", 'w')
+bias.write HEADER
 c['cpus'].each do |cpu|
 	c['vms'].each do |v|
 		log = File.new("netperf_avg.#{IF}#{v}-#{cpu}.log")
@@ -25,12 +27,16 @@ c['cpus'].each do |cpu|
 		avg.write "#{IF}#{v}-cpu#{cpu},#{log.gets}"
 		log = File.new("netperf_min.#{IF}#{v}-#{cpu}.log")
 		log.gets
-		min.write "#{IF}#{v}-cpu#{cpu},#{log.gets}"
+		min_lat, min_tps = log.gets.split(/,/).map do |i| i.to_f end
+		min.write "#{IF}#{v}-cpu#{cpu},#{min_lat},#{min_tps}\n"
 		log = File.new("netperf_max.#{IF}#{v}-#{cpu}.log")
 		log.gets
-		max.write "#{IF}#{v}-cpu#{cpu},#{log.gets}"
+		max_lat, max_tps = log.gets.split(/,/).map do |i| i.to_f end
+		max.write "#{IF}#{v}-cpu#{cpu},#{max_lat},#{max_tps}\n"
+		bias.write "#{IF}#{v}-cpu#{cpu},#{max_lat - min_lat},#{max_tps - min_tps}\n"
 	end
 end
 avg.close
 min.close
 max.close
+bias.close
